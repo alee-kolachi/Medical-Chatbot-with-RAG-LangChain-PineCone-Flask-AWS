48f76cbe1317f32f9217b25cc99f20cdfe939baf
Alee Kolachi <alee.kolachi@gmail.com>
2025-10-06 08:20:11 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical chatbot",
+    version="0.1",
+    author="Ali Kolachi",
+    author_email="alee.kolachi@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
